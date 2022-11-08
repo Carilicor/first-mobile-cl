1a7256f6d9288d28afbec6deb2ae5e5575bb7bb0
Cari Licor <Carilicor@gmail.com>
2022-11-08 15:17:42 -0500

    played with props, state, styling,

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Counter from './components/Counter';
+import Welcome from './components/Welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Welcome firstName={'Cari'} />
+      <Welcome firstName={'Everyone'} />
+      <Text style={styles.bigText}st>I'm amazing 🤪!!</Text>
+      <Counter />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  bigText: {
+    fontSize: '125vw', 
+    color: 'white',
+    textAlign: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
 });
